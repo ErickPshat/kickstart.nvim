ea22ca48b4498034d1a7e11382240ca269bd0261
ErickPshat <t07011966@gmail.com>
2026-05-10 21:24:35 +0300

    godot formatter change

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -38,5 +38,12 @@ return {
       -- You can use 'stop_after_first' to run the first available formatter from the list
       -- javascript = { "prettierd", "prettier", stop_after_first = true },
     },
+    formatters = {
+      gdformat = {
+        command = 'gdformat',
+        args = { '--use-spaces=4', '-' },
+        stdin = true,
+      },
+    },
   },
 }
